e4610e3164388d01b6d85c7e9689ba58774d4431
Vikas Kaushik <Vikas-Kaushik@users.noreply.github.com>
2018-01-19 08:43:21 +0000

    Add files via upload

---
+++ public/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+
+<html lang="en">
+  <head>
+    <?php include './index_head_tag.html'; ?>
+  </head>
+  
+  <body>
+    <div class="site-wrapper">
+      <div class="site-wrapper-inner">
+        <div class="cover-container">
+          <div class="masthead clearfix">
+            <div class="inner">
+              <h3 class="masthead-brand">kaushikvikas.com</h3>
+              <nav>
+                <ul class="nav masthead-nav">
+                  <li class="active"><a href="./">Home</a></li>                  
+                  <li><a href="./articles">Articles</a></li>
+                  <li><a href="./tutorials">Tutorials</a></li>
+                  <li><a href="./resume">My Resume</a></li>
+                  <li><a href="./wilp">WILP</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+
+          <div class="inner cover">
+            <h1 class="cover-heading">Welcome !</h1>
+            <p class="lead"><strong>Motto:</strong> To share worthy practical information and experience in the spirit of engineering goodwill that <em>success is not taken, it is earned.</em></p>
+            <p class="lead">Please visit articles and tutorials section and share further if you find it useful.</p>
+            <p class="lead">
+              <!-- <a href="./resume" class="btn btn-lg btn-default">View My Resume</a> -->
+              <img src="./images/awesome.gif" alt="Awesome!!">
+            </p>
+          </div>
+
+          <div class="mastfoot">
+            <div class="inner">
+              <p><a href="http://in.linkedin.com/pub/vikas-kaushik/35/5a5/79a">Linkedin</a>&nbsp;&nbsp;<a href="https://www.facebook.com/kaushikvikas19">Facebook</a>&nbsp;&nbsp;<a href="https://plus.google.com/u/0/+VikasKaushikonline">Google+</a>&nbsp;&nbsp;<a href="https://www.hackerearth.com/users/kaushikvikas19/">Hackerearth</a>&nbsp;&nbsp;<a href="./sitemap.xml">sitemap</a></p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="./js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
---
+++ public/tutorials/perl_file_handling_snippets.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+
+<html lang="en"><head><?php include '../blog_head_tag.html' ?></head>
+<body>
+<div class="blog-masthead"><div class="container"><?php include '../blog_nav.html' ?></div></div>
+<div class="container"><div class="blog-header"></div><div class="row"><div class="col-sm-8 blog-main">
+
+<!-- Blog Post Content here 
+============================-->
+<?php include './perl_file_handling_snippets_content.html'; ?><!-- End of Post -->
+
+
+<!-- Update Previous and Next links here
+========================================= -->
+<nav><ul class="pager">
+<li><a href="./perl_basics_snippets.php">Previous</a></li>
+<li><a href="./perl_strings_snippets.php">Next</a></li>
+</ul></nav>
+
+</div><!-- /.blog-main --><div class="col-sm-3 col-sm-offset-1 blog-sidebar"><!-- Sidebar --><?php include '../blog_sidebar.html' ?><!-- End sidebar --></div><!-- /.blog-sidebar --></div><!-- /.row -->
+
+<!-- disqus code pasted here -->
+<div id="disqus_thread"></div>
+<script type="text/javascript">
+    /* * * CONFIGURATION VARIABLES * * */
+    var disqus_shortname = 'kaushikvikas';
+    
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function() {
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+<!-- end of disqus -->      
+</div><!-- /.container -->
+<footer class="blog-footer"><?php include '../blog_footer.html'; ?></footer>
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="../js/ie10-viewport-bug-workaround.js"></script>
+    
+<!-- Disqus java script code pasted here
+==========================================-->
+
+<script type="text/javascript">
+    /* * * CONFIGURATION VARIABLES * * */
+    var disqus_shortname = 'kaushikvikas';
+    
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function () {
+        var s = document.createElement('script'); s.async = true;
+        s.type = 'text/javascript';
+        s.src = '//' + disqus_shortname + '.disqus.com/count.js';
+        (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+    }());
+</script>
+</body>
+</html>
